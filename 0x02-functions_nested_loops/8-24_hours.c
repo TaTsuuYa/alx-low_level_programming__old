5e131fd0beedfe8744670f96985ceb22890006b2
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-03-21 13:23:12 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,24 +8,25 @@
 
 void jack_bauer(void)
 {
-	int mins, secs;
+	int mins_digits, mins_decimals, secs_digits, secs_decimals;
 
-	for (mins = 0; mins < 60; mins++)
+	for (mins_deciamls = 0; mins_decimals < 6; mins_decimals++)
 	{
-		for (secs = 0; secs < 60; secs++)
+		for (mins_digits = 0; mins_digits < 10; mins_digits++)
 		{
-			if (mins < 10)
-				_putchar('0');
-			_putchar('0' + mins);
-
-			_putchar(':');
-
-			if (secs < 10)
-				_putchar('0');
-			_putchar('0' + secs);
-
-			_putchar('\n');
+			for (secs_decimals = 0; secs_decimals < 6; secs_decimals++)
+			{
+				for (secs_digits = 0; secs_digits < 10; secs_digits++)
+				{
+					_putchar('0' + mins_decimals);
+					_putchar('0' + mins_digits);
+					_putchar(':');
+					_putchar('0' + secs_decimals);
+					_putchar('0' + secs_digits);
+				}
+			}
 		}
+	}
 
 		if (mins == 23 && secs == 59)
 			return;
