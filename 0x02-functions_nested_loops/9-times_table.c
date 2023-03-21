e4592a2440a43f6f320f6e566c96167bfb4a8f6d
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-03-21 14:12:13 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,20 @@
 
 void times_table(void)
 {
-	int a, b;
+	int a, b, product;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			_putchar('0' + (a * b));
+			product = a * b;
+
+			if (product >= 10)
+			{
+				/* print the decimal place */
+				_putchar('0' + ((int) (product / 10)) % 10);
+			}
+			_putchar('0' + product % 10);
 
 			if (b < 9)
 			{
