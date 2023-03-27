5b6cd4a7b46065d5fbb5af719fa7c8abf44d5c9e
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-03-27 10:21:35 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate length of a string
+ * @s: pointer of the sting's length gets calculated
+ *
+ * Return: int (string length)
+ */
+
+int _strlen(char *s)
+{
+	int size;
+
+	size = sizeof(*s);
+
+	return (size);
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,9 @@ void reset_to_98(int *n);
 /* 1. Don't swap horses in crossing a stream */
 void swap_int(int *a, int *b);
 
+/**
+ * 2. This report, by its very length,
+ * defends itself against the risk of being read
+ */
+int _strlen(char *s);
+
