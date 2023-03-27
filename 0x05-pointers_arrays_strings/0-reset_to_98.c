9ce59c6bf42dcf7689d25c88719bf4b7b498d4d4
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-03-27 10:10:00 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change @n's value to 98
+ * @n: pointer of the variable changing
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,3 @@
+/* 0. 98 Battery st. */
+void reset_to_98(int *n);
+
