0d43e92888af5c2dda12ba3f1728214a1c9602f5
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-03-29 15:40:01 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies @src to @dest at max of @n characters
+ * @src: pointing to sting getting copied
+ * @dest: pointing to string getting copied to
+ * @n: max length
+ *
+ * Return pointer to @dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; ; i++)
+	{
+		if (i >= n)
+			return (dest);
+		else if (src[i] == '\0')
+			break;
+
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,3 +7,6 @@ char *_strcat(char *dest, char *src);
 /* 1. strncat */
 char *_strncat(char *dest, char *src, int n);
 
+/* 2. strncpy */
+char *_strncpy(char *dest, char *src, int n);
+
