44d6739494c65bbdee8184e27637e7c8f1887ee2
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-03 15:13:15 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -18,6 +18,10 @@ char *_strstr(char *haystack, char *needle)
 		if (haystack[i] == needle[0])
 		{
 			for (j = 1; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] == '\0')
+					return (NULL);
+
 				if (haystack[i + j] != needle[j])
 					break;
 
