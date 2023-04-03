5a511c694ca7360beda8b7cbf04eec7fe61c9de6
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-03 07:44:31 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates @c in @s
+ * @s: string looked in
+ * @c: characterd looked for
+ *
+ * Return: address of @c in @s if found, NULL otherwise
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return (s + i);
+
+	return ('\0');
+}
+
