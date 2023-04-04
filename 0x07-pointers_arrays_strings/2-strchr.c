84d2083ae6eec3deceeaf8cac5fe42a38473b45f
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-04 09:37:34 -0700

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,23 +11,9 @@
 
 char *_strchr(char *s, char c)
 {
-	/*
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		if (s[i] == c)
-			return (s + i);
-
-	i++;
-	if (s[i] == c)
-		return (s + i);
-
-	return (NULL);
-	*/
-
 	unsigned int i;
 
-	if (s[0] == '\0' || c == '\0')
+	if (c == '\0')
 		return (NULL);
 
 	for (i = 0; s[i] != '\0'; i++)
