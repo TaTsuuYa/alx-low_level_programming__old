68f6c0a0ba7c37f864eebb496ee1b1bb7dfb1650
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-07 11:48:35 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds two arguments
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: 0 success, 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+		printf("%d\n", 0);
+
+	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+
+	return (0);
+}
+
