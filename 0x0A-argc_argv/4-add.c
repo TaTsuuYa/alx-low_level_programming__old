2060f7ebf3a563ccdc2f94db9dae2b46517a6589
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-07 15:20:01 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - adds two arguments
+ * main - adds argument numbers
  * @argc: argument count
  * @argv: argument array
  *
@@ -11,20 +12,47 @@
 
 int main(int argc, char *argv[])
 {
+	int i, sum;
+
 	if (argc == 1)
 	{
 		printf("%d\n", 0);
 		return (0);
 	}
 
-	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		if (_is_intable(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
 	}
 
-	printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+	printf("%d\n", sum);
 
 	return (0);
+}
+
+/**
+ * _is_intable - checks whether @s can be converted to int
+ * @s: pointer to string
+ *
+ * Return: 1 if convertable, 0 otherwise
+ */
+
+int _is_intable(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+
+	return (1);
 }
 
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+/* 4 - helper */
+int _is_intable(char *s);
 
 #endif
 
