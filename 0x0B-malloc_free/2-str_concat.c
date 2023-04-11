ffebd14385094a3f1518e6b6e1666dadfc5ff7ee
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-11 11:40:42 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - concatenates @s1 and @s2
+ * @s1: pointer toa string
+ * @s2: pointer to another string
+ *
+ * Return: pointer to the conactenation of @s1 and @s2
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int size1, size2, i;
+	char *s;
+
+	for (size1 = 0; s1[size1] != 0; size1++)
+	{}
+
+	for (size2 = 0; s2[size2] != 0; size2++)
+	{}
+
+	s = malloc((sizeof(char) * (size1 + size2)) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size1; i++)
+		s[i] = s1[i];
+
+	for (; i < size2; i++)
+		s[i] = s2[i];
+
+	s[i + 1] = '\0';
+
+	return (s);
+}
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,5 +7,11 @@ char *create_array(unsigned int size, char c);
 /* 1. The woman who has no imagination has no wings */
 char *_strdup(char *str);
 
+/**
+ * 2. He who is not courageous enough to take risks
+ * will accomplish nothing in life
+ */
+char *str_concat(char *s1, char *s2);
+
 #endif
 
