3ce64fbc1d729f966ad1ece1733dbc031905505f
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-12 05:24:58 -0700

    3. If you even dream of beating me you'd better wake up and apologize
    case: malloc failed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,9 +17,16 @@ int **alloc_grid(int width, int height)
 
 	arr = malloc(sizeof(int) * height);
 
+	if (arr == NULL)
+		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(sizeof(int) * width);
+
+		if (arr[i] == NULL)
+			return (NULL);
+
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 	}
