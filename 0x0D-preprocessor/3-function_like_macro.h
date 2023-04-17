b92cd4b6d971906478c95fc72e35de768fa5fd85
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-17 09:08:00 -0700

    3. Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#ifndef ABS(x)
+#define ABD(x) ((x) > 0 ? (x) : ((x) * -1))
+#endif /* indef ABS(x) */
+
+#endif /* indef FUNCTION_LIKE_MACRO_H */
+
