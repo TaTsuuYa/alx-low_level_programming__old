66bb06c0a0b7e581dbe9ff19493fcadea7457d10
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-20 06:49:26 -0700

    1. To be is to be the value of a variable

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints arguments
+ * @separator: pointer to separetor string
+ * @n: number of elements
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		if (separator != NULL)
+			printf("%d%s", va_arg(args, int), separator);
+		else
+			printf("%d", va_arg(args, int));
+
+	va_end(args);
+
+	printf("\n");
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,8 @@
 /* 0. Beauty is variable, ugliness is constant */
 int sum_them_all(const unsigned int n, ...);
 
+/* 1. To be is to be the value of a variable */
+void print_numbers(const char *separator, const unsigned int n, ...);
+
 #endif /* ifndef VARIADIC_FUNCTIONS_H */
 
