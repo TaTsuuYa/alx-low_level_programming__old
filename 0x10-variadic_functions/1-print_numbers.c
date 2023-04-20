9260590ab0ae363e28f936a6e2ec7b40885bc8fb
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-20 07:11:16 -0700

    1. To be is to be the value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
-			if (separator != NULL)
+			if (separator != NULL && i < (n - 1))
 				printf("%d%s", va_arg(args, int), separator);
 			else
 				printf("%d", va_arg(args, int));
