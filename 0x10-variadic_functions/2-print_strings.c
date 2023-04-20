edb545460ee95b2cf8632cd204c1c3ba2b1e68d3
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-20 08:48:05 -0700

    2. One woman's constant is another woman's variable
    fix: typo (arg -> args)
    fix: (char *) -> char *

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,4 @@
-#include "variadic_function.h"
+#include "variadic_functions.h"
 #include <stdio.h>
 
 /**
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (n > 0)
 		for (i = 0; i < n; i++)
 		{
-			s = va_arg(arg, (char *));
+			s = va_arg(args, char *);
 			if (s == NULL)
 				printf("(nil)");
 			else
