2eb038f227831edb6b3f1d588ca8b207672817c0
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-05-03 08:59:48 -0700

    9. Insert
    add: check for negative index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 
+	if (idx < 0)
+		return (NULL);
+	
 	if (idx == 0)
 		return (add_nodeint(head, n));
 
