53f09ebba2b9aa5d44956800f8d0dcda23e3823a
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-05-03 06:38:36 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,3 +37,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	return (new_node);
 }
 
+/**
+ * add_nodeint - adds a node to the beginning
+ * @head: pointer to address of forst element
+ * @n: value of node
+ *
+ * Return: pointer to new list, NULL otheriwse
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
+
