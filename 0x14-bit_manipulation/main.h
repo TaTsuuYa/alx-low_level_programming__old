d46dd6c22c7489475b685607a24be0fbcc7a1197
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-05-04 12:42:54 -0700

    2. 10

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,6 +11,8 @@ unsigned int binary_to_uint(const char *b);
 
 /* 1. 1 */
 void print_binary(unsigned long int n);
+/* 2. 10 */
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif /* #ifndef MAIN_H */
 
