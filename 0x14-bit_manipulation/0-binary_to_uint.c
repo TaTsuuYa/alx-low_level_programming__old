a37eb778ccf498b6c2384526a40ed615b72ec4cf
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-05-04 10:10:01 -0700

    0. 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,14 +9,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, result = 0, len = strlen(b);
+	unsigned int i, result = 0, len = strlen(b), max = sizeof(unsigned int) * 8;
 
 	if (b == NULL || len == 0)
 		return (0);
 
 	for (i = 1; i <= len; i++)
 		if (b[len - i] == '1')
-			result += 1 << (i - 1);
+			if (len - i > max)
+				return (0);
+			else
+				result += 1 << (i - 1);
 		else if (b[len - i] != '0')
 			return (0);
 
