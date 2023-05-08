66ab38ac8ac106e4f8ce29e406e08b2f552d1566
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-05-08 09:49:05 -0700

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content: content of the file
+ *
+ * Return: 1 success, -1 otherwise
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int f, p;
+
+	if (filename == NULL)
+		return (-1);
+
+	f = creat(filename, O_WRONLY);
+	if (f < 0)
+		return (-1);
+
+	p = dprintf(f, "%s", text_content);
+	if (p < 0)
+		return (-1);
+
+	close(f);
+
+	return (1);
+}
+
