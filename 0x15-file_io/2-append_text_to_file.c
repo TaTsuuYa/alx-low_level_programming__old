094a32f497e51ab623ade784b4c0b39b0c1b5956
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-05-08 11:16:32 -0700

    2. Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to file
+ * @filename: the name of the file
+ * @text_content: content appended
+ *
+ * Return: 1 success, -1 otherwise
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int f, a;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+
+	f = open(filename, O_APPEND);
+	if (f < 0)
+		return (-1);
+
+	a = dprintf(f, "%s\n", text_content);
+	if (a < 0)
+		return (-1);
+
+	close(f);
+
+	return (1);
+}
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 /* 1. Under the snow */
 int create_file(const char *filename, char *text_content);
 
+/* 2. Speak gently, she can hear */
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif /* #ifndef MAIN_H */
 
