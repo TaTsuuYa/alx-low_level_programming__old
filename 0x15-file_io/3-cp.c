7de02e73531e04d784ec471470348a05f674732a
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-05-09 02:42:14 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		printf("Usage: cp file_from file_to\n");
+		write(STDERR_FILENO, "Usage: cp file_from file_to\n", strlen("Usage: cp file_from file_to\n"));
 		exit(97);
 	}
 
