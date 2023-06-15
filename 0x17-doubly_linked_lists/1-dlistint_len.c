e156985cdbf0fe1e9dbd1dea4710b118f3439b3a
Taoufik Yahyaoui <TaTsuuYa>
2023-06-15 20:47:20 +0000

    1. List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - number of elements
+ * @h: header
+ * 
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t size = 0;
+    const dlistint_t *current;
+
+    if (h == NULL)
+        return NULL;
+
+    current == h;
+    while (current)
+    {
+        current = current->next;
+        size++;
+    }
+
+    return (size);
+}
