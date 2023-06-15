8f0f8933ca3fae472beddec63808531c8912beec
Taoufik Yahyaoui <TaTsuuYa>
2023-06-15 19:16:20 +0000

    0. Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints elements of list
+ * @h: heater of the list
+ * 
+ * Return: number of elements
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t size = 0;
+    dlistint_t *current;
+
+    if (h == NULL)
+        return (0);
+
+    current = h;
+    while (current)
+    {
+        printf("%d", current->n);
+        current = current->next;
+        size++;
+    }
+
+    return (size);
+}
