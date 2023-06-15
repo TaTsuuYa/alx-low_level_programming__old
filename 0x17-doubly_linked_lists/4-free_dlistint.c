150649718854dbb4e23d28b34d509de374e31a5a
Taoufik Yahyaoui <TaTsuuYa>
2023-06-15 21:53:01 +0000

    4. Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - clears the list
+ * @head: head
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	if (head == NULL)
+		return;
+
+	current = head;
+	while (current)
+	{
+		head = current->next;
+		free(current);
+	}
+}
