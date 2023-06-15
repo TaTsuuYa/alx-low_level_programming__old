d2f14c90bef715ef833970e5ddda27acad8dbf20
Taoufik Yahyaoui <TaTsuuYa>
2023-06-15 21:24:20 +0000

    .\0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -19,7 +19,7 @@ size_t print_dlistint(const dlistint_t *h)
 	current = h;
 	while (current)
 	{
-		printf("%d", current->n);
+		printf("%d\n", current->n);
 		current = current->next;
 		size++;
 	}
