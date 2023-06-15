a038c6ff886ce943afbd81ae6ad9879871d3e5e9
Taoufik Yahyaoui <TaTsuuYa>
2023-06-15 22:09:10 +0000

    6. Sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - calculates the sum of node's data
+ * @head: head
+ *
+ * Return: sum of nodes' data 
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	current = head;
+	while (current)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
