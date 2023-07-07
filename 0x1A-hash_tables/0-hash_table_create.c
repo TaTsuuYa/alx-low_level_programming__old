a06f43fddc10b169ae8f31a3618dc1aee52b4693
Taoufik Yahyaoui <TaTsuuYa>
2023-07-07 22:54:03 +0000

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,17 +1,27 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_create - creates a hash table
-* @size: the size of the table
-*
-* Return: pointer to the created table, or NULL
-*/
+ * hash_table_create - creates a hash table
+ * @size: the size of the table
+ *
+ * Return: pointer to the created table, or NULL
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *htt;
-  hash_node_t *arr;
+	hash_table_t *htt;
+	hash_node_t *arr;
 
-  htt = malloc(sizeof(hash_table_t *) * size);
-  if (htt == NULL)
-    return (NULL);
+	htt = malloc(sizeof(hash_table_t *) * size);
+	if (htt == NULL)
+		return (NULL);
+
+	arr = malloc(sizeof(hash_node_t *) * size);
+	if (arr == NULL)
+		return (NULL);
+
+	htt->size = size;
+	htt->array = arr;
+
+	return (htt);
+}
