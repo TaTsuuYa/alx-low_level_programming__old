63dec7b374656369ea26bea9f4d89d07d456f853
Taoufik Yahyaoui <TaTsuuYa>
2023-07-08 00:57:33 +0000

    4. >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - gets the value
+ * @ht: hash table
+ * @key: the key we looking for
+ *
+ * Return: the value, or NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int ind;
+
+	if (ht == NULL || key == NULL || *key == 0)
+		return (NULL);
+
+	ind = key_index((const unsigned char *)key, ht->size);
+	if (ind >= ht->size)
+		return (NULL);
+
+	if (ind >= ht->size)
+		return (NULL);
+
+	if (node == NULL)
+		return (NULL);
+	return (node->value);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -47,4 +47,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 /* 3. >>> ht['betty'] = 'cool' */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
+/* 4. >>> ht['betty'] */
+char *hash_table_get(const hash_table_t *ht, const char *key);
+
 #endif /* #ifndef HASH_TABLES_H */
