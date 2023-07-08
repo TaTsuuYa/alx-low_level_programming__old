82beefbb948ab33314329f48a6b30ff3ee814a9c
Taoufik Yahyaoui <TaTsuuYa>
2023-07-08 01:02:05 +0000

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -23,6 +23,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ind >= ht->size)
 		return (NULL);
 
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+
 	if (node == NULL)
 		return (NULL);
 	return (node->value);
