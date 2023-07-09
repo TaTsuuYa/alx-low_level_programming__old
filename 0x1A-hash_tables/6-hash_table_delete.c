81cae61dabb1086695b59f2b5546f437906d806d
Taoufik Yahyaoui <TaTsuuYa>
2023-07-09 14:58:04 +0000

    6. >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -22,7 +22,7 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		curr = (ht->array)[i];
-		while (curr)
+		while (curr != NULL)
 		{
 			next = curr->next;
 			free(curr);
